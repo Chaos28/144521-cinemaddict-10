b27458212db2178c614f56e55860e6f03217bb2d
Saire28 <huntergeass@gmail.com>
2020-01-23 11:46:44 +0300

    Выполнил ДЗ 7.1

--- src/components/no-films.js
+++ src/components/no-films.js
@@ -8,6 +8,7 @@ const createNoFilmsDataTemplate = () => {
           </section>`;
 };
 
+
 export default class NoFilms extends AbstractComponent {
   getTemplate() {
     return createNoFilmsDataTemplate();
